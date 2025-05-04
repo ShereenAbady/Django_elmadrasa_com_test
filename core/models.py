9a7fb14de97c068ac4475af6e9717f5528662104
ShereenAbady <shereenabady5@gmail.com>
2025-05-05 00:53:53 +0200

    teachers and students api

---
+++ core/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Teacher(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name='students')
+
+    def __str__(self):
+        return self.name
